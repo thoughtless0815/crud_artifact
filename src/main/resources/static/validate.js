c0be2b508ae612f9ee10b4fa013db8579737395f
Shuvradeb Saha <bsse0815@iit.du.ac.bd>
2019-01-14 14:59:25 +0600

    recorrect validation

--- src/main/resources/static/validate.js
+++ src/main/resources/static/validate.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
 
-    $('#first_form').submit(function(e) {
-        e.preventDefault();
+    function f() {
+
         console.log("Problem");
         var first_name = $('#name').val();
-
+        var flag = false;
         var password = $('#password').val();
-
+        console.log("Problem " + password+" "+password.length);
         $(".error").remove();
 
         if (first_name.length < 1) {
             $('#name').after('<span class="error">This field is required</span>');
+            flag = true;
         }
         if (password.length < 8) {
             $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+            flag = true;
         }
-    });
-
-});
+    return !flag;
+    }

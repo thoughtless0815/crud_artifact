8bed738c83401fcafc416cb477f7b16f0c91a3f4
Tahmid <bsse0801@iit.du.ac.bd>
2019-01-14 15:39:01 +0600

    password changed

--- src/main/resources/static/validate.js
+++ src/main/resources/static/validate.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
-
-    $('#first_form').submit(function(e) {
-        e.preventDefault();
-        console.log("Problem");
-        var first_name = $('#name').val();
-
-        var password = $('#password').val();
-
-        $(".error").remove();
-
-        if (first_name.length < 1) {
-            $('#name').after('<span class="error">This field is required</span>');
-        }
-        if (password.length < 8) {
-            $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
-        }
-    });
-
-});
+// $(document).ready(function () {
+//
+//     $('#first_form').submit(function(e) {
+//         e.preventDefault();
+//         console.log("Problem");
+//         var first_name = $('#name').val();
+//
+//         var password = $('#password').val();
+//
+//         $(".error").remove();
+//
+//         if (first_name.length < 1) {
+//             $('#name').after('<span class="error">This field is required</span>');
+//         }
+//         if (password.length < 8) {
+//             $('#password').after('<span class="error">Password must be at least 8 characters long</span>');
+//         }
+//     });
+//
+// });
